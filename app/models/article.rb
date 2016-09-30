8f6dbe839de705b958f7ecfd98142030d797c5c2
Truong Tuan Manh <tuanmanh.truong@vn.panasonic.com>
2016-09-30 12:04:07 +0000

    Implemented Search function

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,10 @@
 class Article < ApplicationRecord
+  def self.search(search) #self.でクラスメソッドとしている
+    if search # Controllerから渡されたパラメータが!= nilの場合は、titleカラムを部分一致検索
+      filter_letter = '(' + search + ')+' 
+      Article.where(['title ~* ?', "#{filter_letter}"])
+    else
+      Article.all #全て表示。
+    end
+  end
 end
