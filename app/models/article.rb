6502750ea94101ed3dc2a3cf9e957f43373ba077
Manh T. Truong <manhattan.vn@gmail.com>
2016-10-02 22:47:37 +0700

    Better search function

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,8 +4,8 @@ class Article < ApplicationRecord
   validates :title, uniqueness: true
   def self.search(search) #self.でクラスメソッドとしている
     if search # Controllerから渡されたパラメータが!= nilの場合は、titleカラムを部分一致検索
-      filter_letter = '(' + search + ')+' 
-      Article.where(['title ~* ?', "#{filter_letter}"])
+      #filter_letter = '(' + search + ')+' 
+      Article.where(['lower(title) like ?', "%#{search.downcase}%"])
     else
       Article.all #全て表示。
     end
